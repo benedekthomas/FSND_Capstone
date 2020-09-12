df5448a2ce270933130719f0f98863b3ad6286d4
Benedek Tamas <benedek.thomas@gmail.com>
2020-09-12 16:11:34 +0200

    changed folder structure

--- kudosapp/database/manage.py
+++ kudosapp/database/manage.py
@@ -1,16 +1,17 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from api import app
+from app.api import app
 from models import db, Team_Member, Kudos, setup_db
 
-setup_db(app)
+setup_db(api)
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+migrate = Migrate(api, db)
+manager = Manager(api)
 
 manager.add_command('db', MigrateCommand)
 
-
 if __name__ == '__main__':
     manager.run()
+    print(__package__)
+
