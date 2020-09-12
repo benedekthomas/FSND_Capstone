187f0bcbf1f55bc1231db40956333dc3be2d1675
Benedek Tamas <benedek.thomas@gmail.com>
2020-09-13 00:15:04 +0200

    reorganiyed folder structure

--- kudosapp/env/set_db.sh
+++
@@ -1,4 +0,0 @@
-CREATEDB -h 127.0.0.1 -U benedekthomas KUDOS_DB
-export DB_NAME="KUDOS_DB"
-export DB_USER="benedekthomas"
-export DB_PSW="password1"
--- kudosapp/env/startup.sh
+++
@@ -1,2 +0,0 @@
-docker run -itd -e PGPASS=password1 -e POSTGRES_PASSWORD=password1 -e POSTGRES_USER=benedekthomas -e POSTGRES_DB=trivia -p 5432:5432 --name "PGS_Server" --rm postgres
-
