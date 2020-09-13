dfae8eb663bbd575096d1eb2dd72d9d30fe2f48a
Benedek Tamas <benedek.thomas@gmail.com>
2020-09-13 14:11:48 +0200

    try db connection deployed

--- kudosapp.py
+++ kudosapp.py
@@ -1,20 +1,27 @@
 import os
+import json
+import sys
+
 from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+from database.models import db_drop_and_create_all, setup_db, Kudo, Team_Member
 
 app = Flask(__name__)
+try:
+    setup_db(app)
+    print('db setup worked')
+except:
+    print('db connection failed')
+sys.stdout.flush()
 
-@app.route("/", methods=["GET"])
+
+@app.route("/check", methods=["GET"])
 def health_check():
     return jsonify({
         "success": True,
         "message": "Server running"
     })
 
-
-@app.route("/db", methods=["GET"])
-def get_db_details():
-    DATABASE_URL = os.environ['DATABASE_URL']
-    return DATABASE_URL
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
