8149f5531756dba0451e5b8a4f754e1982dca50c
Benedek Tamas <benedek.thomas@gmail.com>
2020-09-13 13:25:31 +0200

    test access to database url with /db endpoint

--- kudosapp.py
+++ kudosapp.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
@@ -8,6 +9,12 @@ def health_check():
         "success": True,
         "message": "Server running"
     })
+
+
+@app.route("/db", methods=["GET"])
+def get_db_details():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    return DATABASE_URL
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
